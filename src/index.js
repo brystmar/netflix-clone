caf03b62d8e114bdecca15600ea375dfc3db1a33
Thomas <brystmar@users.noreply.github.com>
2021-01-30 15:44:39 -0800

    Firebase prep work

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom"
 import "./normalize.css"
 import { GlobalStyles } from "./global-styles"
 import App from "./App"
+import { FirebaseContext } from "./context/firebase"
+import { firebaseConfig } from "./constants/firebaseConfig"
 
+const firebase = window.firebase.initializeApp(firebaseConfig);
+const firebaseAnalytics = window.firebase.analytics();
 
 ReactDOM.render(
     <React.StrictMode>
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
