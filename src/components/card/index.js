a3615e68465d4e1ad268c454bf87e91d4f729ebf
Thomas <brystmar@users.noreply.github.com>
2021-02-15 19:21:19 -0800

    Stubbed out the Card skeleton

---
+++ src/components/card/index.js
@@ -0,0 +1,7 @@
+import React, { useState, useContext, createContext } from 'react';
+
+const FeatureContext = createContext();
+
+export default function Card({ children, ...props }) {
+    return
+}
