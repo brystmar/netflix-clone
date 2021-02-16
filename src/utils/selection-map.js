154d94128ac793bdeb669ebb93357469025c5c7a
Thomas <brystmar@users.noreply.github.com>
2021-02-15 19:03:24 -0800

    More content metadata cleanup

--- src/utils/selection-map.js
+++ src/utils/selection-map.js
@@ -1,16 +1,16 @@
 export default function selectionMap({ tv, movies }) {
     return {
         tv:     [
-            { title: 'Documentaries', data: tv.filter((item) => item.genre === 'documentaries') },
-            { title: 'Comedies', data: tv.filter((item) => item.genre === 'comedies') },
-            { title: 'Children', data: tv.filter((item) => item.genre === 'children') },
+            { title: 'Documentary', data: tv.filter((item) => item.genre === 'documentary') },
+            { title: 'Comedy', data: tv.filter((item) => item.genre === 'comedy') },
+            { title: 'Kids', data: tv.filter((item) => item.genre === 'kids') },
             { title: 'Crime', data: tv.filter((item) => item.genre === 'crime') },
             { title: 'Feel Good', data: tv.filter((item) => item.genre === 'feel-good') },
         ],
         movies: [
-            { title: 'Drama Good', data: movies.filter((item) => item.genre === 'drama') },
+            { title: 'Drama', data: movies.filter((item) => item.genre === 'drama') },
             { title: 'Thriller', data: movies.filter((item) => item.genre === 'thriller') },
-            { title: 'Children', data: movies.filter((item) => item.genre === 'children') },
+            { title: 'Kids', data: movies.filter((item) => item.genre === 'kids') },
             { title: 'Suspense', data: movies.filter((item) => item.genre === 'suspense') },
             { title: 'Romance', data: movies.filter((item) => item.genre === 'romance') },
         ]
