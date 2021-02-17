ad73f81444725b055800a48ad4e6e56b2bdc49c7
Thomas <brystmar@users.noreply.github.com>
2021-02-17 12:27:22 -0800

    Adding Fuse for basic search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from "react"
+import Fuse from "fuse.js"
 import { Header, Loading, Card } from "../components"
 import { FirebaseContext } from "../context/firebase"
 import { SelectProfileContainer } from './profiles'
@@ -28,6 +29,18 @@ export function BrowseContainer({ slides }) {
     useEffect(() => {
         setSlideRows(slides[category]);
     }, [ slides, category ]);
+
+    // Implement basic search w/Fuse
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, { keys: [ 'data.description', 'data.title', 'data.genre' ] });
+        const results = fuse.search(searchTerm).map(({ item }) => item);
+
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+    }, [ searchTerm ])
 
     return profile.displayName ? (
         <>
