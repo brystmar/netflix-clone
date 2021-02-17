fe552501f989467265754b3a8892393abb3072ec
Thomas <brystmar@users.noreply.github.com>
2021-02-17 14:05:36 -0800

    Video player styling & functionality

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, createContext } from 'react';
+import React, { useState, useContext, createContext, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { Container, Overlay, Inner, Button, Close } from "./styles/player"
 
@@ -14,23 +14,32 @@ export default function Player({ children, ...props }) {
     )
 }
 
-Player.Video = function PlayerVideo({ ...props }) {
+Player.Video = function PlayerVideo() {
     const { showPlayer, setShowPlayer } = useContext(PlayerContext);
+    const { playerRef } = useRef(null);
 
     return showPlayer
         ? ReactDOM.createPortal(
             <Overlay onClick={() => setShowPlayer(false)}>
                 <Inner>
-                    <video id="netflix-player" controls>
+                    <video id="video-player" ref={playerRef} aria-label="video-player" controls>
                         <source src="/videos/bunny.mp4" type="video/mp4" />
                     </video>
+                    <Close
+                        aria-label="Close"
+                        onClick={() => {
+                            // Pause playback if a video is currently playing
+                            // playerRef.current.pause();
+                            setShowPlayer(false);
+                        }}
+                    />
                 </Inner>
             </Overlay>,
             document.body
         ) : null
 }
 
-Player.Button = function PlayerButton({ ...props }) {
+Player.Button = function PlayerButton() {
     const { showPlayer, setShowPlayer } = useContext(PlayerContext);
 
     return <Button onClick={() => setShowPlayer(!showPlayer)}>Play</Button>;
--- src/components/player/styles/player.js
+++ src/components/player/styles/player.js
@@ -1,21 +1,86 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-
-`
+export const Container = styled.div``
 
 export const Overlay = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  margin: 0 20px;
 `
 
 export const Inner = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 900px;
+  margin: auto;
 
+  video {
+    height: 100%;
+    width: 100%;
+  }
 `
 
 export const Close = styled.button`
+  position: absolute;
+  right: 15px;
+  top: 15px;
+  width: 22px;
+  height: 22px;
+  background-color: transparent;
+  opacity: 1;
+  border: 0;
+  cursor: pointer;
+  
+  &::before,
+  &::after {
+    position: absolute;
+    left: 10px;
+    top: 0;
+    content: ' ';
+    height: 22px;
+    width: 2px;
+    background-color: #ddd;
+  }
 
+  &::before {
+    transform: rotate(45deg);
+  }
+
+  &::after {
+    transform: rotate(-45deg);
+  }
+  
+  &:hover::before,
+  &:hover::after
+  {
+    background-color: #fff;
+  }
 `
 
 export const Button = styled.button`
+  font-size: 1.125rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 6.5em;
+  height: 2.5em;
+  color: white;
+  background-color: #e50914;
+  border-color: #ff0a16;
+  text-transform: uppercase;
+  padding-left: 0;
+  cursor: pointer;
 
+  &:hover {
+    transform: scale(1.05);
+    background-color: #ff0a16;
+  }
 `
